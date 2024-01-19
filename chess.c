4ec161be86fc5170f30dd477f802beb61dc0b09e
Gustavo-Feijo <gustavofeijo.dev@gmail.com>
2024-01-19 00:49:16 -0300

    Changed the game logic basis, now using a second 2d array to store the possible movements for each selected piece. Capture and movementation for rook, knight and bishop are working well. Still needs to add the other pieces. The code to check for checks is already created and implemented, but only to avoid discovered checks. Improved the comments and readability.

--- chess.c
+++ chess.c
@@ -9,6 +9,13 @@
 #define WHITE -1
 
 typedef bool (*ThreatFunction)(int, int);
+bool isThreatened(int y, int x);
+bool isInCheck();
+bool isHorseThreat(int y, int x);
+bool isDiagonalThreat(int y, int x);
+bool isLineThreat(int y, int x);
+bool isPawnThreat(int y, int x);
+bool isKingThreat(int y, int x);
 
 void drawPieces(ALLEGRO_BITMAP *bitmap);
 void drawValidMove(int y, int x);
@@ -16,17 +23,14 @@ void drawValidMove(int y, int x);
 int *pieceBitmapCoordinates(int chessPieces);
 int *getClickPosition(int y, int x);
 
-bool isInCheck();
-bool isHorseThreat(int y, int x);
-bool isDiagonalThreat(int y, int x);
-bool isLineThreat(int y, int x);
-bool isPawnThreat(int y, int x);
-bool isKingThreat(int y, int x);
-bool isThreatened(int y, int x);
-
 bool isValidMove(int curY, int curX, int nextY, int nextX);
-void horsePattern(int y, int x);
-void horseMove(int curY, int curX, int nextY, int nextX);
+
+void horseMove(int y, int x);
+void bishopMove(int y, int x);
+void rookMove(int y, int x);
+int validateMove(int y, int x);
+
+void clearMovements();
 
 // ENUM with chess notation.
 enum pieces
@@ -52,10 +56,24 @@ int chessBoard[8][8] = {
     {-P, -P, -P, -P, -P, -P, -P, -P},
     {-R, -N, -B, -Q, -K, -B, -N, -R}};
 
+// Creates a 2D array with the available movements.
+bool movements[8][8] =
+    {
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0}};
+
 // Variables to game state control.
 int currentPlayer = WHITE;
+
+//Variable that holds the current selected piece information.
+//[0]= piece value; [1] = y; [2] = x;
 int currentPiece[3] = {0, 0, 0};
-bool isPieceSelected = false;
 
 int main()
 {
@@ -114,35 +132,41 @@ int main()
     while (1)
     {
         al_wait_for_event(eventQueue, &event);
-        if ((event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) && !isPieceSelected)
+        if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN)
         {
             al_get_mouse_state(&mouse);
-
             int *click = getClickPosition(mouse.y, mouse.x);
-            if (chessBoard[click[0]][click[1]] * currentPlayer > 0)
-            {
-                currentPiece[0] = chessBoard[click[0]][click[1]];
-                currentPiece[1] = click[0];
-                currentPiece[2] = click[1];
-            }
 
-            if (currentPiece[0] == N * currentPlayer)
+            if (click[0] != -1)
             {
-                isPieceSelected = true;
-                horsePattern(click[0], click[1]);
+                if (currentPiece[0] == 0 && ((chessBoard[click[0]][click[1]] * currentPlayer) > 0))
+                {
+                    currentPiece[0] = chessBoard[click[0]][click[1]];
+                    currentPiece[1] = click[0];
+                    currentPiece[2] = click[1];
+                    validateMove(click[0], click[1]);
+                    al_flip_display();
+                }
+                else if (movements[click[0]][click[1]] == 1)
+                {
+
+                    chessBoard[currentPiece[1]][currentPiece[2]] = 0;
+                    chessBoard[click[0]][click[1]] = currentPiece[0];
+                    currentPiece[0] = 0;
+                    clearMovements();
+                    drawPieces(piecesBitmap);
+                    al_flip_display();
+                    currentPlayer *= -1;
+                }
+                else
+                {
+                    currentPiece[0] = 0;
+                    clearMovements();
+                    drawPieces(piecesBitmap);
+                    al_flip_display();
+                }
             }
         }
-        else if ((event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) && isPieceSelected)
-        {
-            al_get_mouse_state(&mouse);
-
-            int *nextClick = getClickPosition(mouse.y, mouse.x);
-
-            horseMove(currentPiece[1], currentPiece[2], nextClick[0], nextClick[1]);
-            drawPieces(piecesBitmap);
-            isPieceSelected = false;
-        }
-
         if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
         {
             break; // Exit the loop if the window is closed
@@ -261,7 +285,7 @@ bool isInCheck()
         {
             if (chessBoard[i][j] == K * currentPlayer)
             {
-                return isThreatened(j, i);
+                return isThreatened(i, j);
             }
         }
     }
@@ -294,18 +318,19 @@ bool isHorseThreat(int y, int x)
     for (int i = y - 2; i <= y + 2; i += 4)
     {
         // Checks if it's not offboard, if it is, then continue to the next loop interaction.
-        if (i > 8 || i < 0)
+        if (i >= 8 || i < 0)
             continue;
 
         // Procedes to check the two possible moves.
         for (int j = x - 1; j <= x + 1; j += 2)
         {
 
-            if (j > 8 || j < 0)
+            if (j >= 8 || j < 0)
                 continue;
             // Checks if the position is equal to a knight of the opposite color. Player * -1 indicates that is from the opponent.
             if (chessBoard[i][j] == currentPlayer * N * -1)
             {
+
                 return true;
             }
         }
@@ -313,11 +338,11 @@ bool isHorseThreat(int y, int x)
     // Does the same as above, but for the horizontal options.
     for (int j = x - 2; j <= x + 2; j += 4)
     {
-        if (j > 8 || j < 0)
+        if (j >= 8 || j < 0)
             continue;
         for (int i = y - 1; i <= y + 1; i += 2)
         {
-            if (i > 8 || i < 0)
+            if (i >= 8 || i < 0)
                 continue;
             if (chessBoard[i][j] == currentPlayer * N * -1)
             {
@@ -437,124 +462,190 @@ bool isKingThreat(int y, int x)
     return false;
 }
 
-// Check all possible moves for a given knight and draw the avaible moves.
-void horsePattern(int y, int x)
+
+// Function to draw and enable the rook movement.
+// Can be refactored. A function that only receives the directions and runs the loop for can remove a lot of lines.
+void rookMove(int y, int x)
+{
+    // Direction of each possible move of the rook.
+    int directions[4][2] = {
+        {0, -1}, // Move left
+        {0, 1},  // Move right
+        {1, 0},  // Move down
+        {-1, 0}  // Move up
+    };
+
+    // Loops through each direction, verifying all of them.
+    for (int dir = 0; dir < 4; dir++)
+    {
+
+        // Loops throught each direction until they reach the limits of the board.
+        for (int i = y + directions[dir][0], j = x + directions[dir][1]; (i < 8 && i >= 0) && (j < 8 && j >= 0); i += directions[dir][0], j += directions[dir][1])
+        {
+            // If the position on the direction multiplied by the current player is positive, then it's blocked by a ally piece.
+            if ((chessBoard[i][j] * currentPlayer) > 0)
+            {
+                break;
+            }
+
+            // Checks if the move creates a discovered check, if it does, break the loop.
+            if (!isValidMove(y, x, i, j))
+                break;
+
+            // Change the value on the movements array at the given position to identify that is a valid move.
+            movements[i][j] = 1;
+
+            drawValidMove(i, j);
+        }
+    }
+}
+
+// Function to draw and enable the bishop movement. Essentially works the same as the rookMove, only changing the directions.
+void bishopMove(int y, int x)
+{
+    int directions[4][2] =
+        {
+            {-1, -1},
+            {1, 1},
+            {1, -1},
+            {-1, 1}};
+
+    for (int dir = 0; dir < 4; dir++)
+    {
+        for (int i = y + directions[dir][0], j = x + directions[dir][1]; (i < 8 && i >= 0) && (j < 8 && j >= 0); i += directions[dir][0], j += directions[dir][1])
+        {
+            if ((chessBoard[i][j] * currentPlayer) > 0)
+            {
+                break;
+            }
+            if (!isValidMove(y, x, i, j))
+                break;
+            movements[i][j] = 1;
+            drawValidMove(i, j);
+        }
+    }
+}
+
+// Function to draw and enable the horse movement.
+void horseMove(int y, int x)
 {
     // i = y - 2 checks for the positions below the current, then increases by 4 to check the above positions.
     for (int i = y - 2; i <= y + 2; i += 4)
     {
-        // Checks if it's not offboard, if it is, then continue to the next loop interaction.
+        // Checks if it's not offboard, if it is, then continue to the next loop iteraction.
         if (i >= 8 || i < 0)
             continue;
 
-        // Procedes to check the two possible moves.
+        // Proceds to check the two movement options to the given direction.
         for (int j = x - 1; j <= x + 1; j += 2)
         {
 
+            // Checking for offboard.
             if (j >= 8 || j < 0)
                 continue;
 
+            // Do the validation of the move, if it's not a valid one, just continue to the next loop iteration.
             if (!isValidMove(y, x, i, j))
+            {
                 continue;
+            }
+
+            // Change the value on the movements array at the given position to identify that is a valid move.
+            movements[i][j] = 1;
+
+            // Draw a red dot above the avaible moves.
             drawValidMove(i, j);
         }
     }
-    // Does the same as above, but for the horizontal options.
+    // Does the same as the code above, only changing the direction.
     for (int j = x - 2; j <= x + 2; j += 4)
     {
         if (j >= 8 || j < 0)
             continue;
+
         for (int i = y - 1; i <= y + 1; i += 2)
         {
+
             if (i >= 8 || i < 0)
                 continue;
 
             if (!isValidMove(y, x, i, j))
                 continue;
+
+            movements[i][j] = 1;
+
             drawValidMove(i, j);
         }
     }
-    al_flip_display();
 }
 
-void horseMove(int curY, int curX, int nextY, int nextX)
-{
-    printf("CurX: %d,CurY: %d, NextX: %d,NextY: %d\n",curX,curY,nextX,nextY);
-    fflush(stdout);
-    // i = y - 2 checks for the positions below the current, then increases by 4 to check the above positions.
-    for (int i = curY - 2; i <= curY + 2; i += 4)
-    {
-        // Checks if it's not offboard, if it is, then continue to the next loop interaction.
-        if (i >= 8 || i < 0)
-            continue;
-
-        // Procedes to check the two possible moves.
-        for (int j = curX - 1; j <= curX + 1; j += 2)
-        {
-
-            if (j >= 8 || j < 0)
-                continue;
-
-            if (!isValidMove(curY, curX, i, j))
-                continue;
-            printf("NextY: %d,nextX: %d, i: %d, j: %d\n\n", nextY, nextX, i, j);
-            fflush(stdout);
-            if (nextY == i && nextX == j)
-            {
-                chessBoard[curY][curX] = 0;
-                chessBoard[nextY][nextX] = N * currentPlayer;
-            }
-        }
-    }
-    // Does the same as above, but for the horizontal options.
-    for (int j = curX - 2; j <= curX + 2; j += 4)
-    {
-        if (j >= 8 || j < 0)
-            continue;
-        for (int i = curY - 1; i <= curY + 1; i += 2)
-        {
-            if (i >= 8 || i < 0)
-                continue;
-
-            if (!isValidMove(curY, curX, i, j))
-                continue;
-            printf("NextY: %d,nextX: %d, i: %d, j: %d\n\n", nextY, nextX, i, j);
-            fflush(stdout);
-            if (nextY == i && nextX == j)
-            {
-                chessBoard[curY][curX] = 0;
-                chessBoard[nextY][nextX] = N * currentPlayer;
-            }
-        }
-    }
-}
-
-// Draws the avaible movements.
+// Receives a coordinate and draws a red dot on the respective position on the board.
 void drawValidMove(int y, int x)
 {
     al_draw_filled_circle(100 + (bitmap_size * x), 100 + (bitmap_size * y), 25, al_map_rgb(255, 0, 0));
 }
 
-// Checks if a move creates a validateMove. Returns true if it does.
+// Checks if a move creates a discovered check.
 bool isValidMove(int curY, int curX, int nextY, int nextX)
 {
-    // Checks if the destination is occupied by a enemy piece.
-    if ((chessBoard[curY][curX] * chessBoard[nextY][nextX] < 0) || (chessBoard[nextY][nextX] == 0))
+    // Check if the current position multiplied by the next position is negative.
+    // If it is, implies that they are from different colors, making the capture possible.
+    // If it's a blank square, the multiplication value is going to be 0.
+    if ((chessBoard[curY][curX] * chessBoard[nextY][nextX] <= 0))
     {
-        // Copy the current piece position.
+        // Copy the current position of the selected piece into a temporary array.
         int temp[3] = {chessBoard[curY][curX], curY, curX};
 
+        // Sets the selected square to 0, to enable the checking verification.
         chessBoard[curY][curX] = 0;
-        if (isInCheck(currentPlayer))
+
+        // Assing a variable to the result of the checking of the verification of check.
+        bool isChecked = isInCheck();
+
+        // Return the square for it's original position.
+        chessBoard[temp[1]][temp[2]] = temp[0];
+
+        // Return if the king was put in check, if it was, the position turns to be invalid.
+        return isChecked;
+    }
+
+    // If the previous if conditions are not met, the move is impossible to be performed.
+    return false;
+}
+
+// Receives the piece that was first selected and call the function to validate the possible moves and display them.
+int validateMove(int y, int x)
+{
+    switch (abs(currentPiece[0]))
+    {
+    case K:
+        break;
+    case N:
+        horseMove(y, x);
+        break;
+    case Q:
+        rookMove(y, x);
+        bishopMove(y, x);
+        break;
+    case P:
+        break;
+    case R:
+        rookMove(y, x);
+        break;
+    case B:
+        bishopMove(y, x);
+        break;
+    }
+}
+
+// Clear the 2D array with the current avaible moviments.
+void clearMovements()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
         {
-            chessBoard[temp[1]][temp[2]] = temp[0];
-            return false;
-        }
-        else
-        {
-            chessBoard[temp[1]][temp[2]] = temp[0];
-            return true;
+            movements[i][j] = 0;
         }
     }
-    return false;
 }
